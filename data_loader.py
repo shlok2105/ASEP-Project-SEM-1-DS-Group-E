f9d5239f58cc10ee836e580bef34a48e09c4b4c6
shivapnor-vinay <shivpanorvinay@gmail.com>
2026-02-10 12:55:06 +0530

    removed old python files

--- data_loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-food_dataframes = pd.read_csv("C:\\Users\\Vinay\\Desktop\\ASEP\\nutrition_rules.csv")
-
-goal_dataframes = pd.read_csv("C:\\Users\\Vinay\\Desktop\\ASEP\\goal.csv")
-
-
-
